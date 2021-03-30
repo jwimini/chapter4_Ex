029a402273e26f24d7cfe33562608bb5aee8d3f8
jimin <w2037@e-mirim.hs.kr>
2021-03-30 20:27:22 +0900

    xml Button 연습

---
+++ app/src/main/java/ks/hs/emirim/wlalsdl_04/chapter4_ex/MainActivity.java
@@ -0,0 +1,14 @@
+package ks.hs.emirim.wlalsdl_04.chapter4_ex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
